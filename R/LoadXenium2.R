dbc4a51cec9a4fda63a350ba1e363b81045e423e
Garrett <74611332+gevensen95@users.noreply.github.com>
2025-05-08 13:02:37 -0700

    Update LoadXenium2.R
    Fixed bug in loading data_dir

--- R/LoadXenium2.R
+++ R/LoadXenium2.R
@@ -28,15 +28,15 @@ LoadXenium2 <- function(data_dir, outs = c("matrix", "microns"),
     requireNamespace("R.utils", quietly = TRUE)
   data <- sapply(outs, function(otype) {
     switch(EXPR = otype, matrix = {
-      matrix <- suppressWarnings(Read10X(data.dir = file.path(data.dir,
+      matrix <- suppressWarnings(Read10X(data.dir = file.path(data_dir,
                                                               "cell_feature_matrix/")))
       matrix
     }, centroids = {
       if (has_dt) {
-        cell_info <- as.data.frame(data.table::fread(file.path(data.dir,
+        cell_info <- as.data.frame(data.table::fread(file.path(data_dir,
                                                                "cells.csv.gz")))
       } else {
-        cell_info <- read.csv(file.path(data.dir, "cells.csv.gz"))
+        cell_info <- read.csv(file.path(data_dir, "cells.csv.gz"))
       }
       cell_centroid_df <- data.frame(x = cell_info$x_centroid,
                                      y = cell_info$y_centroid, cell = cell_info$cell_id,
@@ -44,17 +44,17 @@ LoadXenium2 <- function(data_dir, outs = c("matrix", "microns"),
       cell_centroid_df
     }, segmentations = {
       if (has_dt) {
-        cell_boundaries_df <- as.data.frame(data.table::fread(file.path(data.dir,
+        cell_boundaries_df <- as.data.frame(data.table::fread(file.path(data_dir,
                                                                         "cell_boundaries.csv.gz")))
       } else {
-        cell_boundaries_df <- read.csv(file.path(data.dir,
+        cell_boundaries_df <- read.csv(file.path(data_dir,
                                                  "cell_boundaries.csv.gz"), stringsAsFactors = FALSE)
       }
       names(cell_boundaries_df) <- c("cell", "x", "y")
       cell_boundaries_df
     }, microns = {
 
-      transcripts <- arrow::read_parquet(file.path(data.dir, "transcripts.parquet"))
+      transcripts <- arrow::read_parquet(file.path(data_dir, "transcripts.parquet"))
       transcripts <- subset(transcripts, qv >= mols.qv.threshold)
 
       df <- data.frame(x = transcripts$x_location, y = transcripts$y_location,
